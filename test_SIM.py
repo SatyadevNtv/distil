955ab9004a8a85b1bf79dbc433c37eaa55794f7f
Suraj Kothawade <suraj12378@gmail.com>
2021-07-22 17:07:00 -0500

    [test_SIM] Update test script

--- test_SIM.py
+++ test_SIM.py
@@ -7,6 +7,7 @@ from torch.utils.data import Subset
 from torch.utils.data import Dataset
 from torchvision import transforms
 from distil.utils.models.resnet import ResNet18
+import torchvision.models as models
 from PIL import Image
 import sys
 test = sys.argv[1]
@@ -35,15 +36,18 @@ cifar10_dataset = cifar.CIFAR10(root_dir, transform = transform)
 cifar10_test_dataset = cifar.CIFAR10(root_dir, transform = transform, train=False)
 
 cifar10_labeled_dataset = Subset(cifar10_dataset, indices=[x for x in range(100)])
-cifar10_query_dataset = Subset(MyUnlabeledCIFAR10(cifar10_dataset), indices=[x for x in range(100,200)])
-cifar10_private_dataset = Subset(MyUnlabeledCIFAR10(cifar10_dataset), indices=[x for x in range(200,300)])
+cifar10_query_dataset = Subset(cifar10_dataset, indices=[x for x in range(100,200)])
+cifar10_private_dataset = Subset(cifar10_dataset, indices=[x for x in range(200,300)])
 cifar10_unlabeled_dataset = Subset(MyUnlabeledCIFAR10(cifar10_dataset), indices=[x for x in range(300,4000)])
 
-cifar10_model = ResNet18(10).to("cuda")
-cifar10_model.load_state_dict(torch.load("/home/snk170001/bioml/dss/notebooks/weights/cifar10_ResNet18_0.01_50"))
+args = {'batch_size': 20, 'device':'cuda', 'num_partitions':1, 'wrapped_strategy_class': None,'unlabeled':True, 'embedding_type':'features', 'smi_function':'fl2mi', 'scg_function':'flcg', "scmi_function":"flcmi"}
+if(args['embedding_type']=="gradients"):
+    cifar10_model = ResNet18(10).to("cuda")
+if(args['embedding_type']=="features"):
+    cifar10_model =  models.resnet18().to("cuda")
 
+# cifar10_model.load_state_dict(torch.load("/home/snk170001/bioml/dss/notebooks/weights/cifar10_ResNet18_0.01_50"))
 
-args = {'batch_size': 20, 'device':'cuda', 'num_partitions':7, 'wrapped_strategy_class': None, 'smi_function':'fl2mi', 'scg_function':'flcg', "scmi_function":"flcmi"}
 if(test=="scg"):
     args['wrapped_strategy_class'] = SCG
     my_cifar10_strategy = SCG(cifar10_labeled_dataset, cifar10_unlabeled_dataset, cifar10_query_dataset, cifar10_model, 10, args)
@@ -51,7 +55,7 @@ if(test=="scg"):
 elif(test=="smi"):
     args['wrapped_strategy_class'] = SMI
     print(args)
-    my_cifar10_strategy = SMI(cifar10_labeled_dataset, cifar10_unlabeled_dataset, cifar10_private_dataset, cifar10_model, 10, args)
+    my_cifar10_strategy = SMI(cifar10_labeled_dataset, cifar10_unlabeled_dataset, cifar10_query_dataset, cifar10_model, 10, args)
     my_part_cifar10_strategy = PartitionStrategy(cifar10_labeled_dataset, cifar10_unlabeled_dataset, cifar10_model, 10, args, cifar10_query_dataset, None)
 elif(test=="scmi"):
     args['wrapped_strategy_class'] = SCMI
@@ -62,13 +66,13 @@ elif(test=="scmi"):
 my_cifar10_strategy.update_data(cifar10_labeled_dataset, cifar10_unlabeled_dataset)
 my_cifar10_strategy.update_model(cifar10_model)
 
-
 indices = my_cifar10_strategy.select(100)
 print(indices)
 print(len(indices))
 
 # args has 'wrapped_strategy_class' field.
 
-indices = my_part_cifar10_strategy.select(100)
-print(indices)
-print(len(indices))
+part_indices = my_part_cifar10_strategy.select(100)
+print(part_indices)
+print(len(part_indices))
+print("Number of common elements in partition strategy: ", set(indices).intersection(set(part_indices)))
